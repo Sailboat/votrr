bff8054920fd6356f774c4bf77b2693cc9395cf6
Nick Hayden <nickhayden1992@gmail.com>
2013-05-17 17:59:50 +0930

    updated config file/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Votrr::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
